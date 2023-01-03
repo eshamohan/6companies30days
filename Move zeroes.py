ae3d479b8809b9f59c0235289e4625b7bb143dee
eshamohan <76172570+eshamohan@users.noreply.github.com>
2023-01-03 20:50:41 +0530

    Update Move zeroes.py

--- Move zeroes.py
+++ Move zeroes.py
@@ -7,13 +7,11 @@ count=0
 
 for i in range(n):
   if arr[i]==0:
-    print("YO")
     count=count+1
     for j in range(i,n-1):
       arr[j]=arr[j+1]
 for i in range(n-count,n):
   arr[i]=0
-    
-    
+  
 print(count)
 print(arr)
