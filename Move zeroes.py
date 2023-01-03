ce4368c08039c7aeedf3a13f919a418fe4046487
eshamohan <76172570+eshamohan@users.noreply.github.com>
2023-01-03 20:49:45 +0530

    Move Zeroes
    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
    Note that you must do this in-place without making a copy of the array.

---
+++ Move zeroes.py
@@ -0,0 +1,19 @@
+n = int(input("Enter number of terms: "))
+arr=[]
+print("enter elements:")
+for i in range (n):
+  arr.append(int(input()))
+count=0
+
+for i in range(n):
+  if arr[i]==0:
+    print("YO")
+    count=count+1
+    for j in range(i,n-1):
+      arr[j]=arr[j+1]
+for i in range(n-count,n):
+  arr[i]=0
+    
+    
+print(count)
+print(arr)
